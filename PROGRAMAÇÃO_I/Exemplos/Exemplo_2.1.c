d8292cee162e6267b59043450a35953918ee1091
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-02 19:34:08 -0300

    4.1

--- PROGRAMAÇÃO_I/Exemplos/Exemplo_2.1.c
+++ PROGRAMAÇÃO_I/Exemplos/Exemplo_2.1.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+/* 	Programador: Ronaldo Queiroz
+	Data: 02/11/2023
+	Programa para calcular o cubo de um número */
 main()
 { 
-int num,c; 
+int num,c; // C guarda o cubo do número
 printf("Cubo de um numero\n\n"); 
 printf("Digite o numero: "); 
 scanf("%d",&num); 
 c = num*num*num; 
 printf("\nCubo de %d = %d",num,c); 
-getche(); }
+getche(); 
+}
---
+++ PROGRAMAÇÃO_I/Exemplos/Exemplo_4.1.c
@@ -0,0 +1,11 @@
+//Commandos de saída de dados
+
+printf("Uma limha");
+
+printf("Uma linha\nDuas linhas");
+
+printf("Os numeros sao: %d e %d\n",7,8);
+
+printf("Caruaru esta a %f Km de Recife", dist);
+
+printf("%d%% de %d = %f\n", p, x, x*(p/100.0));
