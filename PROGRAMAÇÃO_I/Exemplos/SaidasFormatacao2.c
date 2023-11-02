321e69f61f4b2e4a9a219946d76ceaf2c9aa2958
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-02 19:57:18 -0300

    Update SaidasFormatacao2.c

--- PROGRAMAÇÃO_I/Exemplos/SaidasFormatacao2.c
+++ PROGRAMAÇÃO_I/Exemplos/SaidasFormatacao2.c
@@ -3,8 +3,8 @@
 main()
 {	printf("Teste de Arredondamento\n\n");
 	printf("R$ %.3f!\n\n", 1234.5689);
-	printf("R$ %.1f!\n\n", 1234.5689);
-	printf("R$ %.2f!\n\n", 1234.5689);
+	printf("R$ %.1f!\n\n", 1234.5632);
+	printf("R$ %.2f!\n\n", 1234.5632);
 	printf("R$ %.2f!\n\n", 1234.5652);
 	getche();
 }
