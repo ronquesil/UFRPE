ca8d85de06d362eb6285bf38155eb8f6f6217576
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-02 18:35:46 -0300

    Create Exemplo_2.1.c
    Exemplo 2.1: Programa C – Cálculo do cubo de um número

---
+++ PROGRAMAÇÃO_I/Exemplos/Exemplo_2.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{ 
+int num,c; 
+printf("Cubo de um numero\n\n"); 
+printf("Digite o numero: "); 
+scanf("%d",&num); 
+c = num*num*num; 
+printf("\nCubo de %d = %d",num,c); 
+getche(); }
