838e51da42ea9867216c8267e4db725726e4fd32
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-02 21:03:02 -0300

    Update EstruturaSelecao_switch.c

--- PROGRAMAÇÃO_I/Videoaulas/EstruturaSelecao_switch.c
+++ PROGRAMAÇÃO_I/Videoaulas/EstruturaSelecao_switch.c
@@ -10,10 +10,10 @@ main()
 	printf("Quantidade: ");
 	scanf("%d", &quant);
 	switch(cod)
-	{	case 102: printf("\n\nTotal a pagar: R$%.2f\n", quant*5.3;break);
-		case 103: printf("\n\nTotal a pagar: R$%.2f\n", quant*6.0;break);
-		case 104: printf("\n\nTotal a pagar: R$%.2f\n", quant*3.2;break);
-		case 105: printf("\n\nTotal a pagar: R$%.2f\n", quant*2.5;break);
+	{	case 102: printf("\n\nTotal a pagar: R$%.2f\n", quant*5.3);break;
+		case 103: printf("\n\nTotal a pagar: R$%.2f\n", quant*6.0);break;
+		case 104: printf("\n\nTotal a pagar: R$%.2f\n", quant*3.2);break;
+		case 105: printf("\n\nTotal a pagar: R$%.2f\n", quant*2.5);break;
 		default: printf("\n\nCódigo de produto inválido!\n");break;
 	}
 	getche();
