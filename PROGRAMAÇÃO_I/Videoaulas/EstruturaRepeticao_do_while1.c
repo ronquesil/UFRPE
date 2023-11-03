aba27109ee58d455ee787e47d54edb6e4e1b741e
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-02 21:48:51 -0300

    Update EstruturaRepeticao_do_while1.c

--- PROGRAMAÇÃO_I/Videoaulas/EstruturaRepeticao_do_while1.c
+++ PROGRAMAÇÃO_I/Videoaulas/EstruturaRepeticao_do_while1.c
@@ -1,23 +1,27 @@
-#include<conio.h>
-#include<stdio.h>
+#include <conio.h>
+#include <stdio.h>
 #include <locale.h>
-//Calcula bônus do Cliente
-main()
-{	setlocale(LC_ALL, "Portuguese");
-	float valor;
-	int cod, op;
-	printf("Loja Compre Tudo\n\n");
-	do
-	{	printf("Código do Cliente: ");
-		scanf("%d", &cod);
-		printf("Valor das compras: ");
-		scanf("%f", &valor);
-	if (valor<5000)
-		printf("\n\nValor do bônus R$ %.2f\n", valor*0.1);
-	else
-		printf("\n\nValor do bônus R$ %.2f\n", valor*0.15);
-		printf("\n\nDeseja continuar(1-sim / 2-não)?:");
-		scanf("%d",&op);
-	} while (op==1)
-	getche();
+// Calcula bônus do Cliente
+int main() // Changed 'main()' to 'int main()' to specify return type
+{
+    setlocale(LC_ALL, "Portuguese");
+    float valor;
+    int cod, op;
+    printf("Loja Compre Tudo\n\n");
+    do
+    {
+        printf("Código do Cliente: ");
+        scanf("%d", &cod);
+        printf("Valor das compras: ");
+        scanf("%f", &valor);
+        if (valor < 5000)
+            printf("\n\nValor do bônus R$ %.2f\n", valor * 0.1);
+        else
+            printf("\n\nValor do bônus R$ %.2f\n", valor * 0.15);
+        printf("\n\nDeseja continuar(1-sim / 2-não)?: \n\n");
+        scanf("%d", &op);
+    } while (op == 1); // Added missing semicolon
+
+    return 0; // Return statement added
 }
+
