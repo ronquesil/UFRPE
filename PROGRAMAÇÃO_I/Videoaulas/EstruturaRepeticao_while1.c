de031307aba50d5ccd8af17b624b56d1467876b2
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-02 21:34:10 -0300

    Create EstruturaRepeticao_while1.c

---
+++ PROGRAMAÇÃO_I/Videoaulas/EstruturaRepeticao_while1.c
@@ -0,0 +1,24 @@
+#include<conio.h>
+#include<stdio.h>
+#include <locale.h>
+//Calcula bônus do Cliente
+main()
+{	setlocale(LC_ALL, "Portuguese");
+	float valor;
+	int cod, cont;
+	printf("Loja Compre Tudo\n\n");
+	cont = 1;
+	while (cont<=5)
+	{	printf("Código do Cliente %d: ", cont);
+	scanf("%d", &cod);
+	printf("Valor das compras: ");
+	scanf("%f", &valor);
+	if (valor<5000)
+		printf("\n\nValor do bônus R$ %.f\n", valor*0.1);
+	else
+		printf("\n\Valor do bonus R$ %.2f\n", valor*0.15);
+	printf("\n\nTecle ENTER para continuar...\n\n");
+	getche();
+	cont++;
+	}
+}
