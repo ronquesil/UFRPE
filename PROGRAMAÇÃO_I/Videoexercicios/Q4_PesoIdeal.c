39dacb94ca3a04541f48fb136cde852e6ad8dd3a
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-07 16:16:38 -0300

    Create Q4_PesoIdeal.c

---
+++ PROGRAMAÇÃO_I/Videoexercicios/Q4_PesoIdeal.c
@@ -0,0 +1,9 @@
+#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+/*cálculo do peso ideal a partir da altura e orientação sexual */
+main()
+{	setlocale(LC_ALL, "Portuguese");
+main()
+	
+}
