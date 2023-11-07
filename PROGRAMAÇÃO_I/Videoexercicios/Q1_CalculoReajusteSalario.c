d5b37bb368488e9734b5b021a01da436175ed565
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-06 22:14:07 -0300

    Update Q1_CalculoReajusteSalario.c

--- PROGRAMAÇÃO_I/Videoexercicios/Q1_CalculoReajusteSalario.c
+++ PROGRAMAÇÃO_I/Videoexercicios/Q1_CalculoReajusteSalario.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <conio.h>
+#include <stdio.h>
 #include <locale.h>
 /*cálculo do salário */
 main()
-{	setlocale (LC_ALL, "Portuguese");
+{	setlocale(LC_ALL, "Portuguese");
 	float sm, pr, ns; //sm = salário mensal, pr = percentual de reajuste, ns = novo salário
 	printf("\nCálculo do salário do funcionário\n\n");
 	printf("\nSalário mensal: R$ ");
@@ -11,6 +11,6 @@ main()
 	printf("\nPercentual de reajuste: ");
 	scanf("%f", &pr);
 	ns = sm + (sm*pr/100);
-	printf("\n\nNovo Salário: R$ $.2f\n", ns);
-	getche
+	printf("\n\nNovo Salário: R$ %.2f\n", ns);
+	getche();
 }
