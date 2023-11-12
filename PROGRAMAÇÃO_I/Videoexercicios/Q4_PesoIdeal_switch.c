67be039991f11c48e8819e2f663d39b7d4064e56
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-12 20:07:12 -0300

    Update Q4_PesoIdeal_switch.c

--- PROGRAMAÇÃO_I/Videoexercicios/Q4_PesoIdeal_switch.c
+++ PROGRAMAÇÃO_I/Videoexercicios/Q4_PesoIdeal_switch.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
-//#include <locale.h>
 /*cálculo do peso ideal a partir da altura e orientação sexual */
 main()
 {	
@@ -13,25 +12,11 @@ main()
 	scanf("%f", &altura);
 	printf("\nQual o seu sexo? (Digite 1 - Masculino / 2 - Feminino) ");
 	scanf("%d", &sexo);
-	if (sexo==1)
-		pesoideal = (72.7*altura)-58;
-	else
-		pesoideal = (62.1*altura)-44.7;
+	switch(sexo)		
+	{
+		case 1: pesoideal = (72.7*altura)-58; break;
+		case 2:	pesoideal = (62.1*altura)-44.7; break;
+	}
 	printf("\n\nO seu peso ideal é %.2f kg \n", pesoideal);
 	getche();
 }
-/*main ()
-{	float altura, pesoideal;
-	int sexo;
-	printf("Cálculo de Peso Ideal\n\n");
-	printf("Altura em metros(m): ");
-	scanf("%f", &altura);
-	printf("\nSexo(1-mas/2-fem): ");
-	scanf("%d",&sexo);
-	if(sexo==1)
-		pesoideal = (72.7*altura)-58;
-	else
-		pesoideal = (62.1*altura)-44.7;
-	printf("\n\nSeu peso ideal: %.2fkg \n", pesoideal);
-	getche();
-}*/
